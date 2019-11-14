953c850b75becfacf3505fa818b6b90ce8c6e700
Robert Kowalski <rok@kowalski.gd>
2019-11-14 11:23:50 +0100

    fxGrenache: make grape instances optional
    useful when there is no additional Grape instance to pass in,
    e.g. when running as "standalone" in a dev setup

--- utils.js
+++ utils.js
@@ -9,3 +9,13 @@ exports.getGrapeApiPort = getGrapeApiPort
 function getGrapeApiPort (grapeHelperInstance) {
   return grapeHelperInstance.grapes[0].conf.api_port
 }
+
+exports.getGrapeApiUrl = getGrapeApiUrl
+function getGrapeApiUrl (grapes) {
+  if (typeof grapes === 'string') {
+    return grapes
+  }
+
+  const p = getGrapeApiPort(this.grapes)
+  return 'http://127.0.0.1:' + p
+}
